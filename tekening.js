c54aa0caf9f9ceaa50319d19537c8c8b4be8e806
tessvanbussel <38736040+tessvanbussel@users.noreply.github.com>
2018-06-14 14:13:24 +0200

    + tekening.js

--- tekening.js
+++ tekening.js
@@ -1,30 +1,57 @@
-var xCar = 50;
+var radius = 10;
+var xSize = 600;
+var ySize = 200;
+var xSpeed1 = 2;
+var ySpeed1 = 3;
+var xBall1 = 21;
+var yBall1 = 21;
+var xSpeed2 = 5;
+var ySpeed2 = 3;
+var xBall2 = 50;
+var yBall2 = 50;
+var xSpeed3 = 1;
+var ySpeed3 = 3;
+var xBall3 = 70;
+var yBall3 = 10;
 
-function setup() {
- 
-  createCanvas(400, 180); 
- 
-  background(220); 
+function setup(){
+  createCanvas(xSize, ySize);
 }
 
-function draw() {
-    //Geen randen om de auto
-    noStroke(); 
-    //Kies de opvulkleur (rgb)
-    fill(255, 220, 115); 
-    //Parameters: x, y, breedte, hoogte
-    rect(xCar, 100, 110, 20); 
-    rect(xCar + 10, 78, 70, 40);
-    // Wielen
-    //Witte randen voor de wielen
-    stroke(255); 
-    //Iets dikkere randen
-    strokeWeight(2); 
-    fill(12, 66, 66);
-    //Parameters: x, y, breedte, hoogte
-    ellipse(xCar + 25, 121, 24, 24); 
-    ellipse(xCar + 80, 121, 24, 24);
-    stroke(0); //Zwarte weg
-    //Parameters: x1,y1,x2,y2 
-    line(0, 121+12, 200, 121+12);
-};
+function draw(){
+  background(0);
+  if (xBall1 > width - radius || xBall1 < radius){
+      xSpeed1 = -xSpeed1;
+  }
+  if (yBall1 > height - radius || yBall1 < radius){
+      ySpeed1 = -ySpeed1;
+  }
+  xBall1 += xSpeed1;
+  yBall1 += ySpeed1;
+   r=random(255)
+  g=random(255)
+  b=random(255)
+  fill(r,g,b)
+
+  ellipse(xBall1, yBall1, 2*radius, 2*radius);
+  
+  if (xBall2 > width - radius || xBall2 < radius){
+      xSpeed2 = -xSpeed2;
+  }
+  if (yBall2 > height - radius || yBall2 < radius){
+      ySpeed2 = -ySpeed2;
+  }
+  xBall2 += xSpeed2;
+  yBall2 += ySpeed2;
+     r=random(255)
+  g=random(255)
+  b=random(255)
+  fill(r,g,b)
+  ellipse(xBall2, yBall2, 2*radius, 2*radius);
+  
+  if (xBall3 > width - radius || xBall3 < radius){
+      xSpeed3 = -xSpeed3;
+  }
+  if (yBall3 > height - radius || yBall3 < radius){
+      ySpeed3 = -ySpeed3;
+  }
